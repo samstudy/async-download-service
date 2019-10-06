4a1188bb44c9e0f4590035f7629a5dbe4ca60684
samstudy <samat.study@gmail.com>
2019-10-06 13:15:14 +0600

    Renamed a valiue,refactored func

--- server.py
+++ server.py
@@ -17,30 +17,27 @@ def get_args():
 
 async def archivate(request,photos_dir,is_enable_log,delay):
     folder = photos_dir + request.match_info['archive_hash']
-    if os.path.exists(folder):
-        response = web.StreamResponse()
-        response.headers['Content-Disposition'] = 'attachment; filename="archive.zip"'
-        await response.prepare(request)
-        zip_process = await asyncio.create_subprocess_exec(
-            'zip', '-r', '-', folder,
-            stdout=asyncio.subprocess.PIPE,
-            stderr=asyncio.subprocess.PIPE)
-        try:
-            while True:
-                archive_chunk = await zip_process.stdout.readline()
-                if bool(is_enable_log):
-                    logging.debug('Sending archive chunk ...')
-                if not archive_chunk:
-                    break
-                await response.write(archive_chunk)
-                await asyncio.sleep(delay)
-        except asyncio.CancelledError:
-            raise    
-        finally:
-            zip_process.kill()
-            response.force_close
-    else:
+    if not os.path.exists(folder):
         return web.HTTPNotFound(text='Архив не существует или был удален')
+    response = web.StreamResponse()
+    response.headers['Content-Disposition'] = 'attachment; filename="archive.zip"'
+    await response.prepare(request)
+    zip_process = await asyncio.create_subprocess_exec(
+        'zip', '-r', '-', folder,
+        stdout=asyncio.subprocess.PIPE,
+        stderr = asyncio.subprocess.PIPE)
+    try:
+        while True:
+            archive_chunk = await zip_process.stdout.readline()
+            if bool(log_enabled):
+                logging.debug('Sending archive chunk ...')
+            if not archive_chunk:
+                break
+            await response.write(archive_chunk)
+            await asyncio.sleep(delay)
+    finally:
+        zip_process.kill()
+        response.force_close
     return response
 
 
@@ -54,13 +51,15 @@ def main():
     params = get_args()
     app = web.Application()
     photos_folder = params.photos_dir
-    is_enable_log = params.log
+    log_enabled = params.log
     delay = params.delay
-    chunk_archivate = functools.partial(archivate,photos_folder,is_enable_log,delay )
+    chunk_archivate = functools.partial(archivate,photos_folder,log_enabled,delay )
     app.add_routes([
         web.get('/', handle_index_page),
         web.get('/archive/{archive_hash}/', chunk_archivate),
         ])
     web.run_app(app)
+
+
 if __name__ == '__main__':
     main()
